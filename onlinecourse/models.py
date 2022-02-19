c868fdbd59927d52b3ab85800c0e3a79a94a766c
moosaei <mohammad.moosaei@gmail.com>
2022-02-19 14:03:16 +0100

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -107,7 +107,7 @@ class Enrollment(models.Model):
     # question grade/mark
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question_text = models.TextField()
+    text = models.TextField()
     grade = models.IntegerField(default=0)
   
     # <HINT> A sample model method to calculate if learner get the score of the question
@@ -129,9 +129,9 @@ class Question(models.Model):
     # Other fields and methods you would like to design
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.TextField()
+    text = models.TextField()
     is_correct = models.BooleanField(default=False)
-    
+
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
